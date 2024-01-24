903e8dc1fe2d648164d68fdd971a6064812c522f
Nathan Lambertson <lambertones@gmail.com>
2024-01-24 08:36:46 -0600

    Add method to recursively add new nodes to tree

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -9,11 +9,28 @@ class BinarySearchTree
   def insert(score, title)
     if @root_node.nil?
       @root_node = Node.new(score, title)
+      return 0 # depth
     else
-      if @root_node.score > score
-        @root_node.left_node = Node.new(score, title)
+      return insert_node(@root_node, score, title, 0)
+    end
+  end
+
+  private
+
+  def insert_node(current_node, score, title, depth)
+    if score < current_node.score
+      if current_node.left_node.nil?
+        current_node.left_node = Node.new(score, title)
+        return depth + 1
       else
-        @root_node.right_node = Node.new(score, title)
+        insert_node(current_node.left_node, score, title, depth + 1)
+      end
+    elsif score > current_node.score
+      if current_node.right_node.nil?
+        current_node.right_node = Node.new(score, title)
+        return depth + 1
+      else
+        insert_node(current_node.right_node, score, title, depth + 1)
       end
     end
   end
