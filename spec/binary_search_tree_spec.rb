935904877822bf803c5d99301438c4d44607b5a0
Nathan Lambertson <lambertones@gmail.com>
2024-01-23 21:57:23 -0600

    Start insert method tests

--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -14,4 +14,31 @@ RSpec.describe BinarySearchTree do
     expect(tree).to be_a(BinarySearchTree)
     expect(tree.root_node).to be(nil)
   end
+
+  describe "methods" do
+    it "can add a root node to the tree with #insert" do
+      tree = BinarySearchTree.new
+
+      tree.insert(47, "Toys")
+
+      expect(tree.root_node.score).to eq(47)
+      expect(tree.root_node.title).to eq("Toys")
+      expect(tree.root_node.left_node).to be(nil)
+      expect(tree.root_node.right_node).to be(nil)
+    end
+
+    it  "can add a left or right node with #insert" do
+      tree = BinarySearchTree.new
+
+      tree.insert(47, "Toys")
+    
+      expect(tree.root_node.score).to eq(47)
+      expect(tree.root_node.title).to eq("Toys")
+
+      tree.insert(23, "Bill and Ted")
+
+      expect(tree.root_node.left_node.score).to eq(23)
+      expect(tree.root_node.left_node.title).to eq("Bill and Ted")
+    end
+  end
 end
