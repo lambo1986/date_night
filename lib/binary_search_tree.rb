a18e09c3e88445e90c0db6669b71cfcd496d534f
Nathan Lambertson <lambertones@gmail.com>
2024-01-23 21:57:09 -0600

    Begin insert method

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -5,4 +5,16 @@ class BinarySearchTree
   def initialize
     @root_node = nil
   end
+
+  def insert(score, title)
+    if @root_node.nil?
+      @root_node = Node.new(score, title)
+    else
+      if @root_node.score > score
+        @root_node.left_node = Node.new(score, title)
+      else
+        @root_node.right_node = Node.new(score, title)
+      end
+    end
+  end
 end
